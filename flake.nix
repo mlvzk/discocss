e8724d76f66475cddef1654f29c8457f6f32e9d4
mlvzk <mlvzk@protonmail.com>
2021-05-09 03:33:16 +0200

    format

--- flake.nix
+++ flake.nix
@@ -5,26 +5,25 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    (flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ self.overlay ];
-            config.allowUnfree = true;
-          };
-        in
-        {
-          packages.discocss = pkgs.discocss;
-
-          defaultPackage = self.packages.${system}.discocss;
-        })
-    ) // {
-      overlay = final: prev: {
-        discocss = prev.callPackage (import ./.) { };
-      };
-
-      hmModule = import ./hm-module.nix;
+  outputs = { self, nixpkgs, flake-utils }: {
+    overlay = final: prev: {
+      discocss = prev.callPackage (import ./.) { };
     };
+
+    hmModule = import ./hm-module.nix;
+  } // (flake-utils.lib.eachDefaultSystem
+    (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+          config.allowUnfree = true;
+        };
+      in
+      {
+        packages.discocss = pkgs.discocss;
+
+        defaultPackage = self.packages.${system}.discocss;
+      })
+  );
 }
